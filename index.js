b3bcfe99b434844098d49a0c82f93f1da8ef01c1
nightblood <nightblood@localhost.localdomain>
2021-09-21 05:40:32 +0100

    cors

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const PORT = process.env.PORT || 4000
 //Connect DB 
 DB()
 
-app.use(cors());
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
